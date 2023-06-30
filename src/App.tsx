742fda2f7ade676611ca2dc6a9375812ade462d4
Joakim Karlsson <joakim.t.karlsson.1985@gmail.com>
2023-06-30 23:08:42 +0200

    Setup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Imports
+import React, { useState } from "react";
+import { convertBinaryToDecimal } from "./Utils/convertBinToDec";
+import { theme } from "../src/Themes/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import "./App.css";
+import CardComponent from "./Components/Card/CardComponent";
+import SnackbarComponent from "./Components/SnackbarComponent/SnackbarComponent";
 
 function App() {
+  // Initializes and manages input and output values,
+  // and whether or not snackbar is open (opens when user types anything else than 0 or 1)
+  const [outputValue, setOutputValue] = useState("0");
+  const [inputValue, setInputValue] = useState("");
+  const [snackbarIsOpen, setSnackbarIsOpen] = useState(false);
+
+  // Function to handle changes to the input field, filtering input and converting to decimal
+  const handleChange = (textFieldVal: string): void => {
+    if (/[^01]/.test(textFieldVal)) {
+      setSnackbarIsOpen(true);
+    }
+    // Filter out anything that is not a 1 or 0
+    const filteredValue = textFieldVal.replace(/[^01]/g, "");
+    setInputValue(filteredValue);
+    setOutputValue(convertBinaryToDecimal(filteredValue));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Returns a theme provider and card component with necessary props
+    <ThemeProvider theme={theme}>
+      <CardComponent
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        outputValue={outputValue}
+        setOutputValue={setOutputValue}
+        handleChange={handleChange}
+        variant={"elevation"}
+        raised={true}
+      />
+      <SnackbarComponent
+        snackbarIsOpen={snackbarIsOpen}
+        setSnackbarIsOpen={setSnackbarIsOpen}
+      />
+    </ThemeProvider>
   );
 }
 
